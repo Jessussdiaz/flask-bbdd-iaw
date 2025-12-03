2892e47ca71e7c326017870ad49f43c5d2038d9c
Alejandroluna05 <a24lupaal@iesgrancapitan.org>
2025-12-03 12:55:02 +0100

    Add files via upload
    Añadida funcion Home y home.html con lista de tablas para darle despues clic y que te lleve a una pagina de marcas(seleccionando la tabla marcas) y coches(seleccionando coches)

--- aplicacion.py
+++ aplicacion.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 import sqlite3
 # Autores: Alejandro Luna Paredes y Jesús Díaz Mata
 # Fecha 03/12/2025
@@ -9,3 +9,15 @@ def get_db_connection():
     conn = sqlite3.connect('coches.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+@app.route('/')
+def home():
+    conn = get_db_connection()
+    entidades = conn.execute("""
+    SELECT name FROM sqlite_master 
+    WHERE type='table' AND name NOT LIKE 'sqlite_%';
+""").fetchall()
+    conn.close()
+    return render_template('home.html', entidades=entidades)
+
+app.run(debug=True)
